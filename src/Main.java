358650116e14c40f8330f66e062f945b76e9b7f5
Ronald Ayala <real.ayala99@gmail.com>
2024-09-03 20:27:00 -0600

    modifique el mensaje en clase main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String miTexto = "Bienvenidos a Java Developer";
+        System.out.println("Hola, " + miTexto);
+       // System.out.println("Hello world!"
     }
 }
